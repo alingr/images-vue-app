36dd6d3d9acd0b7c221355f4434f5cbfe0628838
alingr <34002917+alingr@users.noreply.github.com>
2020-05-02 20:53:04 +0300

    Add files via upload

---
+++ src/api/imgur.js
@@ -0,0 +1,43 @@
+import qs from 'qs';
+import axios from 'axios';
+
+//const CLIENT_ID = '905b87134c3ecd6';
+//const CLIENT_ID = '41dfa21fd5ded3e';
+//const CLIENT_ID = '635e3f2b4be94e9';
+const CLIENT_ID = '9292e97e8e29577';
+
+const ROOT_URL = 'https://api.imgur.com';
+
+export default {
+  login() {
+    const querystring = {
+      client_id: CLIENT_ID,
+      response_type: 'token'
+    };
+
+    window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
+      querystring
+    )}`;
+  },
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  },
+  uploadImages(images, token) {
+    const promises = Array.from(images).map(image => {
+      const formData = new FormData();
+      formData.append('image', image);
+
+      return axios.post(`${ROOT_URL}/3/image`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    });
+
+    return Promise.all(promises);
+  }
+};
